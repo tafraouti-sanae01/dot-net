02611269fdc7a6c25d896cb1246258cacd111602
tafraouti-sanae01 <tafraouti.sanae1@gmail.com>
2026-02-16 07:59:40 +0000

    02-01-d

--- src/Program.cs
+++ src/Program.cs
@@ -15,26 +15,26 @@ class Program
         #region Mise en forme
         var prix = 9.5m;
 
-        // Console.WriteLine("Le montant est " + prix + " EUR.");
+        Console.WriteLine($"Le montant est {prix:F2} EUR.");
         #endregion
 
         #region Interprétation "Parsing"
         var saisie = "2.40 CHF";
 
-        if (decimal.TryParse(saisie, out var chf))
+        if (decimal.TryParse(saisie.Substring(0, saisie.IndexOf(" ")), out var chf))
         {
-            Console.WriteLine($"Le montant lu est : {chf:.00} CHF");
+            Console.WriteLine($"Le montant lu est : {chf:F2} CHF");
         }
         else
         {
-            // Console.Error.WriteLine("Impossible d'extraire le montant");
+            Console.Error.WriteLine("Impossible d'extraire le montant");
         }
         #endregion
 
         #region Manipulation/Comparaison
         if (adresse.ToUpper().Contains("GENÈVE"))
         {
-            // Console.WriteLine("Adresse genevoise.");
+            Console.WriteLine("Adresse genevoise.");
         }
         else
         {
